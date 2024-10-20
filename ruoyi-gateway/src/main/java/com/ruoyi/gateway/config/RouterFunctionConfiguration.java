d8b852f62cbe884b01cb6d4c52df5324f30194af
wenjie.yu <2868719112@qq.com>
2024-10-21 00:36:26 +0800

    修改配置文件，本地运行

--- ruoyi-gateway/src/main/java/com/ruoyi/gateway/config/RouterFunctionConfiguration.java
+++ ruoyi-gateway/src/main/java/com/ruoyi/gateway/config/RouterFunctionConfiguration.java
@@ -11,12 +11,16 @@ import com.ruoyi.gateway.handler.ValidateCodeHandler;
 
 /**
  * 路由配置信息
+ * 简单的 Spring WebFlux 配置，用于定义基于函数式编程的路由
+ * 定义了一个 Spring WebFlux 的路由配置，当收到一个 HTTP GET 请求到 /code 路径，且请求的 Accept 头部为 text/plain 时，该请求将由 validateCodeHandler 处理
  * 
  * @author ruoyi
  */
 @Configuration
 public class RouterFunctionConfiguration
 {
+    //private ValidateCodeHandler validateCodeHandler; 这行代码定义了一个私有字段 validateCodeHandler，类型为 ValidateCodeHandler。
+    // 当 Spring 容器创建 RouterFunctionConfiguration 的实例时，它会自动查找并注入一个 ValidateCodeHandler 类型的 bean 到这个字段中。
     @Autowired
     private ValidateCodeHandler validateCodeHandler;
 
